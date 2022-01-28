3c04c25091969196b26828d6c748901ed5521308
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-01-28 15:29:47 +0500

    adds

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db.models import Sum
 from django.shortcuts import reverse
 from django_countries.fields import CountryField
-from .views import SignUpView
+from django.views.generic import CreateView
 
 CATEGORY_CHOICES = (
     ('S', 'Shirt'),
@@ -28,7 +28,7 @@ ADDRESS_CHOICES = (
 #     score = models.IntegerField(default=0);
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(SignUpView, on_delete=models.CASCADE)
+    user = models.OneToOneField(CreateView, on_delete=models.CASCADE)
     stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
     one_click_purchasing = models.BooleanField(default=False)
 
