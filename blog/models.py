8d3dbacd19c1f8e36057590e681d7b290f34244d
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-01-28 15:26:17 +0500

    adds

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,10 @@
-from views import SignUpView
 from django.db.models.signals import post_save
 from django.conf import settings
 from django.db import models
 from django.db.models import Sum
 from django.shortcuts import reverse
 from django_countries.fields import CountryField
-
+from .views import SignUpView
 
 CATEGORY_CHOICES = (
     ('S', 'Shirt'),
