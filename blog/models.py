be2f887d93d7275db7999744bc51a8428d23c7cb
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-01-28 15:23:48 +0500

    adds

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from views import SignUpView
 from django.db.models.signals import post_save
 from django.conf import settings
 from django.db import models
@@ -23,10 +24,12 @@ ADDRESS_CHOICES = (
     ('S', 'Shipping'),
 )
 
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     score = models.IntegerField(default=0);
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(SignUpView, on_delete=models.CASCADE)
     stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
     one_click_purchasing = models.BooleanField(default=False)
 
